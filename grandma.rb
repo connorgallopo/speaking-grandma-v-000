52587b3fe531d829172481bd62deb92c589f076c
Connor Gallopo <connor.gallopo@me.com>
2017-11-08 16:46:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    "HUH? SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   elsif
     phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
